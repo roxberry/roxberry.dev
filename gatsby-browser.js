a6b2b663fbf7917f66f699d1f5e82fa60eebba50
Mark Roxberry <mark@un2.tech>
2021-07-27 15:08:31 -0400

    Keep Prism, removed line numbers until responsive solution

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,5 +5,5 @@ import 'normalize.css/normalize.css';
 import "typeface-raleway";
 
 //code syntax color and line numbers
-// require("prismjs/themes/prism-okaidia.css")
+require("prismjs/themes/prism-okaidia.css")
 // require("prismjs/plugins/line-numbers/prism-line-numbers.css")
