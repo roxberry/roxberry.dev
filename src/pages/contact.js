462295d48f5d759aebc2a7823463204df044b1a2
Mark Roxberry <mroxberry@healthverity.com>
2022-01-18 21:45:35 -0500

    fix: revert contact style

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
+import * as contactStyles from "../styles/contact.module.scss"
 
 const ContactPage = () => {
   return (
